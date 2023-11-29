c0c19bb904fef8107792838aab65fe932f38b4ad
kouzber123 <tom.nguyen@windowslive.com>
2023-11-29 10:42:13 +0200

    autopep

--- unitTesting/testAutomation.py
+++ unitTesting/testAutomation.py
@@ -1,30 +1,35 @@
 import unittest
 import test
+
+
 class TestMathMethods(unittest.TestCase):
 
     def testaddReturnFour(self):
-       shouldBeFour =  test.add(2,2)
-       answer = 4
-       self.assertEqual(shouldBeFour, answer)
+        shouldBeFour = test.add(2, 2)
+        answer = 4
+        self.assertEqual(shouldBeFour, answer)
+
     def testReturnWrongEqual(self):
-        four = test.add(2,2)
+        four = test.add(2, 2)
         answer = 5
         self.assertNotEqual(four, answer)
 
     def testMultiplyCorrectAnswer(self):
-        m = test.multiply(2,2)
+        m = test.multiply(2, 2)
         self.assertEqual(m, 4)
+
     def testMultiplyIncorrectAnswer(self):
-        m = test.multiply(2,2)
+        m = test.multiply(2, 2)
         self.assertNotEqual(m, 5)
 
     def testPowerCorrect(self):
-        p = test.power(2,8)
+        p = test.power(2, 8)
         self.assertEqual(p, 256)
+
     def testPowerInCorrect(self):
-        p = test.power(2,7)
+        p = test.power(2, 7)
         self.assertNotEqual(p, 256)
+
 
 if __name__ == '__main__':
     unittest.main()
-
